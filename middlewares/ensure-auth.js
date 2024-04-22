70076d81c431b4a67fa47f01ecd1622effe08dcd
ali.raza@algorepublic.com <ali.raza@algorepublic.com>
2024-04-22 18:41:34 +0500

    protect routes

--- middlewares/ensure-auth.js
+++ middlewares/ensure-auth.js
@@ -1,34 +1,41 @@
 const jwt = require('jsonwebtoken')
 const { errorResponse } = require('../utils/response')
 
-const ensureAuth = async (req, res, next) => {
-  const connection = req.dbConnection
+const ensureAuth = (roles) => {
+  return async (req, res, next) => {
+    const connection = req.dbConnection
 
-  const token = req?.header('Authorization')
+    const token = req?.header('Authorization')
 
-  if (token) {
-    try {
-      let decoded = null
+    if (token) {
       try {
-        decoded = await jwt.verify(token, process.env.APP_JWT_KEY)
+        let decoded = null
+        try {
+          decoded = await jwt.verify(token, process.env.APP_JWT_KEY)
+        } catch (err) {
+          return errorResponse(res, 'Invalid authorization token')
+        }
+
+        if (!decoded?._id) throw new Error('Invalid authorization token')
+
+        const query = { _id: decoded._id }
+        if (roles) {
+          query.roles = roles
+        }
+
+        const user = await connection.model('User').findOne(query)
+
+        if (!user) throw new Error('Invalid authorization token')
+
+        req.currentUser = user
+
+        next()
       } catch (err) {
-        return errorResponse(res, 'Invalid authorization token')
+        return errorResponse(res, err?.message)
       }
-
-      if (!decoded?._id) throw new Error('Invalid authorization token')
-
-      const user = await connection.model('User').findOne({ _id: decoded._id })
-
-      if (!user) throw new Error('Invalid authorization token')
-
-      req.currentUser = user
-
-      next()
-    } catch (err) {
-      return errorResponse(res, err?.message)
+    } else {
+      return errorResponse(res, 'Authorization token is missing')
     }
-  } else {
-    return errorResponse(res, 'Authorization token is missing')
   }
 }
 
--- routes/api/v1/admin/index.js
+++ routes/api/v1/admin/index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const authRouter = require('./auth')
 const usersRouter = require('./users')
+const { ensureAuth } = require('../../../../middlewares/ensure-auth')
 
 const router = express.Router()
 
 router.use('/auth', authRouter)
-router.use('/users', usersRouter)
+router.use('/users', ensureAuth('admin'), usersRouter)
 
 module.exports = router
