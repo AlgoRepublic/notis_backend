1433e3099598fe85b0cf5314fe5bd63696da95cd
zeeshan-algo <zeeshan.ashraf@algorepublic.com>
2024-05-20 14:22:23 +0500

    email functionality added

---
+++ controllers/api/v1/admin/contact/create.js
@@ -0,0 +1,34 @@
+const { aysncMiddleware } = require('../../../../../middlewares/async');
+const { sendEmail } = require('../../../../../utils/Email');
+
+
+
+
+const create = aysncMiddleware(async (req, res, next) => {
+
+
+
+    const { email, message } = req.body
+
+    const emailOptions = {
+        to: email,
+        cc: 'info@algorepublic.com',
+        subject: 'Contact Email',
+        text: message,
+    }
+
+    await sendEmail(res, emailOptions);
+
+
+
+
+
+
+
+
+
+})
+
+
+
+module.exports = create
---
+++ routes/api/v1/admin/contact.js
@@ -0,0 +1,12 @@
+
+
+
+
+const express = require('express')
+
+const { create } = require('../../../../controllers/api/v1/admin/contact')
+const app = express.Router()
+
+app.post('/', create)
+
+module.exports = app
