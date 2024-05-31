0c9474ee879483bbf62ffdfe84ddc9a5dbe78923
zeeshan-algo <zeeshan.ashraf@algorepublic.com>
2024-05-31 16:47:59 +0500

    contact us functionality added

---
+++ controllers/api/v1/app/contact/send.js
@@ -0,0 +1,37 @@
+const { aysncMiddleware } = require('../../../../../middlewares/async')
+const { sendEmail } = require('../../../../../utils/email')
+
+const send = aysncMiddleware(async (req, res, next) => {
+    const { name, email, subject, message } = req.body
+
+
+
+    const Textmessage = `<div style="color:#000000"> 
+                            <div style="display:flex;">
+                                <div style="min-width:8%"><strong>Name : </strong></div>
+                                <div>${name}</div>
+                            </div>
+                            <div style="display:flex;">
+                                <div style="min-width:8%"><strong>Email : </strong></div>
+                                <div>${email}</div>
+                           </div>
+                            <div style="display:flex;">
+                                <div style="min-width:8%"><strong>Subject : </strong></div>
+                                <div>${subject}</div>
+                            </div>
+                            <div style="display:flex;">
+                                <div style="min-width:8%"><strong>Message : </strong></div>
+                                <div>${message}</div>
+                            </div>     
+                        <div>
+    `;
+
+    const emailOptions = {
+        subject: 'Notis App Contact ',
+        text: Textmessage,
+    }
+
+    await sendEmail(res, emailOptions)
+})
+
+module.exports = send
--- routes/api/v1/app/index.js
+++ routes/api/v1/app/index.js
@@ -3,6 +3,7 @@ const appsRouter = require('./apps')
 const jobsRouter = require('./jobs')
 const devicesRouter = require('./devices')
 const searchesRouter = require('./searches')
+const contactRouter = require('./contact')
 
 const router = express.Router()
 
@@ -10,5 +11,6 @@ router.use('/apps', appsRouter)
 router.use('/jobs', jobsRouter)
 router.use('/devices', devicesRouter)
 router.use('/searches', searchesRouter)
+router.use('/contact', contactRouter)
 
 module.exports = router
