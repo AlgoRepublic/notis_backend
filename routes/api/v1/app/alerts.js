0c89507f9b5073c533ab0a2cafe9b6bbfde6552d
ali.raza@algorepublic.com <ali.raza@algorepublic.com>
2024-06-04 18:12:59 +0500

    clear alerts api

--- routes/api/v1/app/alerts.js
+++ routes/api/v1/app/alerts.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const ensureSubdomain = require('../../../../middlewares/ensure-subdomain')
 const ensureDevice = require('../../../../middlewares/ensure-device')
-const { list, update } = require('../../../../controllers/api/v1/app/alerts')
+const {
+  list,
+  update,
+  clear,
+} = require('../../../../controllers/api/v1/app/alerts')
 
 const app = express.Router()
 
 app.get('/', ensureSubdomain, ensureDevice, list)
 app.put('/:_id', ensureSubdomain, ensureDevice, update)
+app.post('/clear', ensureSubdomain, ensureDevice, clear)
 
 module.exports = app
