0ee3f0d4a407950e768d0fb39a30ef789578e4d6
zeeshan-algo <zeeshan.ashraf@algorepublic.com>
2024-06-14 18:04:28 +0500

    admin contact api update

--- controllers/api/v1/admin/contact/send.js
+++ controllers/api/v1/admin/contact/send.js
@@ -2,20 +2,16 @@ const { aysncMiddleware } = require('../../../../../middlewares/async')
 const { sendEmail } = require('../../../../../utils/email')
 
 const send = aysncMiddleware(async (req, res, next) => {
-  const { name, email, subject, message } = req.body
+  const { first_name, last_name, email, message } = req.body
   const Textmessage = `<div style="color:#000000"> 
                             <div style="display:flex;">
                                 <div style="min-width:8%"><strong>Name : </strong></div>
-                                <div>${name}</div>
+                                <div>${first_name} ${last_name}</div>
                             </div>
                             <div style="display:flex;">
                                 <div style="min-width:8%"><strong>Email : </strong></div>
                                 <div>${email}</div>
                            </div>
-                            <div style="display:flex;">
-                                <div style="min-width:8%"><strong>Subject : </strong></div>
-                                <div>${subject}</div>
-                            </div>
                             <div style="display:flex;">
                                 <div style="min-width:8%"><strong>Message : </strong></div>
                                 <div>${message}</div>
