49873c3bd74b0007f429d6956b2b69c71711eb8e
zeeshan-algo <zeeshan.ashraf@algorepublic.com>
2024-06-14 17:59:16 +0500

    admin contact api update

--- controllers/api/v1/admin/contact/send.js
+++ controllers/api/v1/admin/contact/send.js
@@ -2,13 +2,30 @@ const { aysncMiddleware } = require('../../../../../middlewares/async')
 const { sendEmail } = require('../../../../../utils/email')
 
 const send = aysncMiddleware(async (req, res, next) => {
-  const { email, message } = req.body
+  const { name, email, subject, message } = req.body
+  const Textmessage = `<div style="color:#000000"> 
+                            <div style="display:flex;">
+                                <div style="min-width:8%"><strong>Name : </strong></div>
+                                <div>${name}</div>
+                            </div>
+                            <div style="display:flex;">
+                                <div style="min-width:8%"><strong>Email : </strong></div>
+                                <div>${email}</div>
+                           </div>
+                            <div style="display:flex;">
+                                <div style="min-width:8%"><strong>Subject : </strong></div>
+                                <div>${subject}</div>
+                            </div>
+                            <div style="display:flex;">
+                                <div style="min-width:8%"><strong>Message : </strong></div>
+                                <div>${message}</div>
+                            </div>     
+                        <div>
+    `
 
   const emailOptions = {
-    to: email,
-    cc: 'info@algorepublic.com',
-    subject: 'Contact Email',
-    text: message,
+    subject: 'Notis App Contact ',
+    text: Textmessage,
   }
 
   await sendEmail(res, emailOptions)
